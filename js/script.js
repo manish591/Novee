498f0beb7df8c24b4d3956b1afcc7319485f014c
Manish Devrani <manishdevrani777@gmail.com>
2022-02-19 13:07:07 +0530

    refactor-removed typo, refactor js code

--- js/script.js
+++ js/script.js
@@ -3,9 +3,5 @@ const btn = document.querySelector('.listing-header__right');
 
 
 btn.addEventListener('click', e => {
-    if(listingFilter.classList.contains('filter-list__open')) {
-        listingFilter.classList.remove('filter-list__open')
-    } else {
-        listingFilter.classList.add('filter-list__open')
-    }
+    listingFilter.classList.toggle('filter-list__open');
 })
